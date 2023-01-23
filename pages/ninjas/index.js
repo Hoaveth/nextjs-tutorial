2e468169e3f0ae27cb2aa3d4abf8b20ed1f198f1
Hoaveth <hoaveth.gaming@gmail.com>
2023-01-23 21:32:27 +0800

    Initial nextjs setup

---
+++ pages/ninjas/index.js
@@ -0,0 +1,12 @@
+const Ninjas = () => {
+  return (
+    <div>
+      <h1>Ninja Listing</h1>
+      <p>
+        Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
+      </p>
+    </div>
+  );
+};
+
+export default Ninjas;
