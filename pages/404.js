869810cd1e59d938ffb231b03962a26b496ee35e
Hoaveth <hoaveth.gaming@gmail.com>
2023-01-25 12:52:42 +0800

    Added redirection to homepage for 404 pages

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, [router]);
+
   return (
-    <div>
+    <div className="not-found">
       <h1>Oooopss...</h1>
       <h2>Page cannot be found</h2>
       <p>
